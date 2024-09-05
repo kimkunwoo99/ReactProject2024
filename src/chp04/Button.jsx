54ac2ae93442e52976e015829c78ca650867e766
kimkunwoo99 <kimkunwoo99@gmail.com>
2024-09-05 15:33:09 +0900

    React Element

--- src/chp04/Button.jsx
+++ src/chp04/Button.jsx
@@ -10,5 +10,5 @@ function Button(props){
         </button>
     )
 }
-
+//
 export default Button;
--- src/chp04/ConfirmDialog.jsx
+++ src/chp04/ConfirmDialog.jsx
@@ -11,5 +11,5 @@ function ConfirmDialog(props){
         </div>
     )
 }
-
+//
 export default ConfirmDialog;
