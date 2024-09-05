c075847bce97b559f9d679799c7ea57e6189d76b
kimkunwoo99 <kimkunwoo99@gmail.com>
2024-09-05 16:22:14 +0900

    React Element

---
+++ src/chp04/Clock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Clock(props){
+    return(
+        <div>
+            <h1>Clock Test</h1>
+            <div className='clock'>{new Date().toLocaleTimeString()}</div>
+        </div>
+    );
+}
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './chp03/Book.css'
 import './chp04/Button.css'
+import './chp04/Clock.css'
 import reportWebVitals from './reportWebVitals';
 import ConfirmDialog from "./chp04/ConfirmDialog";
+import Clock from "./chp04/Clock";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ConfirmDialog/>
-  </React.StrictMode>
-);
+setInterval(()=>{
+    root.render(
+        <React.StrictMode>
+            <Clock/>
+        </React.StrictMode>
+        );
+    }
+    ,1000
+)
+
 
 // If you want to start measuring performance in your app, pass a function//
 // to log results (for example: reportWebVitals(console.log))
