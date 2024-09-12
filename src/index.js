ecbcfb2e2d5ffc580cbf97b75ae599635742b633
kimkunwoo99 <kimkunwoo99@gmail.com>
2024-09-12 16:00:34 +0900

    React 5장 component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './chp03/Book.css'
-import './chp04/Button.css'
-import './chp04/Clock.css'
+// import './chp03/Book.css'
+// import './chp04/Button.css'
+// import './chp04/Clock.css'
 import reportWebVitals from './reportWebVitals';
 import ConfirmDialog from "./chp04/ConfirmDialog";
 import Clock from "./chp04/Clock";
+import CommentList from "./chp05/CommentList";
 
 
 
@@ -14,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(()=>{
     root.render(
         <React.StrictMode>
-            <Clock/>
+            <CommentList/>
         </React.StrictMode>
         );
     }
